246b8931111d0e5c987eabcc00c5bf8d731b77f2
Ri087 <ri087@MacBook-Air-de-Ri087.local>
2024-12-11 16:20:03 +0100

    feat(db): create migration v1 resolve #3

---
+++ database/migrations/1733928687607_create_shrooms_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'shrooms'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().notNullable()
+      table.integer('affected_user_id').unsigned().notNullable()
+      table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE')
+      table.foreign('affected_user_id').references('id').inTable('users').onDelete('CASCADE')
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
