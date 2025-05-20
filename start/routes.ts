3ff44166c5acbc56afd2fee424e71adc02b39f69
Ri087 <ri087@MacBook-Air-de-Ri087.local>
2025-05-20 12:59:43 +0200

    siuu

--- start/routes.ts
+++ start/routes.ts
@@ -8,7 +8,6 @@
 */
 
 import router from '@adonisjs/core/services/router'
-import { middleware } from '#start/kernel'
 
 const AuthController = () => import('#controllers/auth_controller')
 const GameController = () => import('#controllers/game_controller')
